2a6c16e463e4caefcc833bc83fdebf5c0d4cfbc6
Pakize Bozkurt <pakiozihak@gmail.com>
2022-11-10 12:23:29 +0000

    Created path.js

--- calculator.js
+++ calculator.js
@@ -2,10 +2,10 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", function(req,res){
-    res.send("Hello world!");
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(3000, function(){
-    console.log("Server started on part 3000");
+app.listen(3000, function () {
+  console.log("Server started on part 3000");
 });
